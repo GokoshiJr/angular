56a932c36217947477710230613b1196af810433
julio <julioagonzalez18@gmail.com>
2024-04-14 17:14:45 -0400

    feat [my-first-app]: Add alert components

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers', /* by html tag */
+  // selector: '.app-servers', /* by html class */
+  // selector: '[app-servers]', /* by html attribute */
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+
+}
