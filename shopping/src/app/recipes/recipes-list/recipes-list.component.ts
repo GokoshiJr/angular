8a36b4d2096d8faa1fcf7f5c6e60eab2615750d8
julio <julioagonzalez18@gmail.com>
2024-06-30 13:00:56 -0400

    [shopping]: recipes-list

--- shopping/src/app/recipes/recipes-list/recipes-list.component.ts
+++ shopping/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipes-list.component.css'
 })
 export class RecipesListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is simply test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe_writing_700-94d33d1.jpg?quality=90&webp=true&resize=300,272')
+  ];
 
 }
