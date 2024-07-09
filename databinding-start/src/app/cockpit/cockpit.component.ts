1080207e64c177e5ca55ca7c3f43dd7f16274c9c
Julio Gonzalez <julioagonzalez18@gmail.com>
2024-07-09 09:05:22 -0400

    [databinding-start]: getting access to the template & dom with @ViewChild()

--- databinding-start/src/app/cockpit/cockpit.component.ts
+++ databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,19 +9,21 @@ export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output("bpCreated") bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // newServerName = '';
-  newServerContent = '';
+  /* newServerContent = ''; */
+  @ViewChild("serverContentInput") serverContentInput: ElementRef;
 
   onAddServer(nameInput: HTMLInputElement) {
+    console.log(this.serverContentInput.nativeElement.value);
     this.serverCreated.emit({
-      serverName: nameInput.value, 
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.bluePrintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 }
